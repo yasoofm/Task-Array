753e077ab3c3265134773c937c3d40746e3acd59
Yousef T Y Y A Mandani <83332@kfh.com>
2024-01-03 09:04:45 +0300

    submission

---
+++ Arrays.java
@@ -0,0 +1,8 @@
+public class Arrays{
+    public static void main(String[] args){
+        double[] weather = {30.2, 21, 25.3, 40, 29.9};
+        weather[2] = 50;
+        for(int i = 0; i < weather.length; i++){
+        System.out.println(weather[i]);
+        }
+}
